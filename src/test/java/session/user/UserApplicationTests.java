2698ddff8e4ddb4c02040d5c0c9418f3637ebdd6
jiyeon0926 <yeoni9094@naver.com>
2025-02-27 21:55:11 +0900

    init

---
+++ src/test/java/session/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package session.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
